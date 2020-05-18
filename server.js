a055b6661fcecadda76273c80acb530bf231bb8e
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-18 12:21:55 +0900

    Declare Timestamp Function

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+function timestamp(str) {
+    return {};
+}
+
 app.get("/", (req, res) => res.send("Hello, World!"));
+app.get("/api/timestamp/:date_string?", function (req, res) {
+    res.json(
+        timestamp(req.params.date_string)
+    );
+});
 
 app.listen(port, function() {
     console.log(`Timestamp Microservice listening on port ${port}`);
